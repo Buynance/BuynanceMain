18592f28bdf3aafb340b9599f70e5cf51f508d59
Tran Hai Thanh <thanhtran@cnc.vn>
2014-02-09 11:08:07 +0700

    Add 1 formula + minor fixes

--- app/models/profitability.rb
+++ app/models/profitability.rb
@@ -13,10 +13,10 @@
 #
 
 class Profitability < ActiveRecord::Base
-  attr_accessible :monthly_cash_collection_amount, :gross_profit_margin, 
+  attr_accessible :monthly_cash_collection_amount, :gross_profit_margin, :total_month_fully_profitable_again,
     :total_monthly_bills, :daily_merchant_cash_advance, :projected_monthly_profit
 
-  validates :monthly_cash_collection_amount, :gross_profit_margin, 
+  validates :monthly_cash_collection_amount, :gross_profit_margin, :total_month_fully_profitable_again,
     :total_monthly_bills, :daily_merchant_cash_advance, :projected_monthly_profit, presence: true
 
   before_validation :calculate_daily_profit
@@ -25,5 +25,16 @@ class Profitability < ActiveRecord::Base
     @pay_back = 0.2
     self.gross_profit_margin = (monthly_cash_collection_amount - total_monthly_bills)/monthly_cash_collection_amount
     self.projected_monthly_profit = ((monthly_cash_collection_amount - total_monthly_bills)/30 - (30 * daily_merchant_cash_advance)/20.8)*30
+
+    # total_month_fully_profitable_again
+    if projected_monthly_profit < 0
+      val1 = projected_monthly_profit < -1 ? 6 : projected_monthly_profit > 1 ? 0 : 0
+      val2 = (projected_monthly_profit * 6 / (monthly_cash_collection_amount - total_monthly_bills)).abs
+      self.total_month_fully_profitable_again = (val1 + val2).ceil
+    else
+      self.total_month_fully_profitable_again = 0
+    end
+
+
   end
 end
