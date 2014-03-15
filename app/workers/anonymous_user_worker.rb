db0a43e1e765f0beac2540992fb831d69a9e573a
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-03-15 11:58:44 -0400

    deleted stuff

--- app/workers/anonymous_user_worker.rb
+++
@@ -1,9 +0,0 @@
-class AnonymousUserWorker
-  include Sidekiq::Worker
-  sidekiq_options retry: false
-  
-  def perform(ip, profitability_id)
-    profitability = Profitability.find_by_id(profitability_id)
-  	profitability.create_anonymous_user(ip)  
-  end
-end
