b83eb6154a6b0f0bcab7610f3e529cc70e19f755
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-03-15 15:40:18 -0400

    background workers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Buynance::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => 'buynance.com' }
+
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
