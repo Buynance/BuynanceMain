a21e94c5805d38765d9ddaa06064cda41a49ef7d
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-03-16 23:24:57 -0400

    set gemfile delayed job web

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ActionMailer::Base
   def qualified_signup(business)
   	@business = business
     mail :subject => "You have a new qualified person signup to buynance",
-         :to      => "edwin@buynance.com",
+         :to      => "jay@buynance.com",
          :from    => "Team Buynance <noreply@buynance.com>"
   end
 end
