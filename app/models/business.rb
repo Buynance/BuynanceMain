bf802b59d1a410d1162a8d377e6f906acb42e01d
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-03-19 16:13:48 -0400

    change rate of offers

--- app/models/business.rb
+++ app/models/business.rb
@@ -106,8 +106,10 @@ class Business < ActiveRecord::Base
     average = Offer.get_three_months_average(self.earned_one_month_ago,
         earned_two_months_ago, earned_three_months_ago)
     days = 60
+    days = 120 if @business.approximate_credit_score_range >= 4  
     for n in 0...amount 
-      factor_rate = Offer.get_random_rate(1.32, 1.38)
+      factor_rate = Offer.get_random_rate(1.36, 1.48)
+      factor_rate = Offer.get_random_rate(1.32, 1.38) if @business.approximate_credit_score_range >= 4  
       daily_rate = Offer.get_random_rate(0.145, 0.15)
 
       daily_payback = Offer.get_daily_payback(self.average_daily_balance_bank_account, daily_rate)
