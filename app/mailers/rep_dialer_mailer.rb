d2cf830dbf01847d7b400a1dac9bf5b466997a3c
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-09-02 09:55:39 -0400

    frontend and banckend

--- app/mailers/rep_dialer_mailer.rb
+++ app/mailers/rep_dialer_mailer.rb
@@ -2,7 +2,7 @@ class RepDialerMailer < ActionMailer::Base
 
 	def representative_acceptance(representative)
 		@representative = representative
-		mail :subject => "Congratulation, you have been accepted to join the Buynance family!",
+		mail :subject => "Welcome to the family.",
          :to      => representative.email,
          :from    => "Team Buynance <noreply@buynance.com>"
 	end
