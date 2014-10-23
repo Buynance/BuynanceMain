56b55dfa3c698bf4b206525ad9f3a626381e3c7e
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-10-23 03:05:40 -0400

    Buynance Family

--- app/models/discovery_type.rb
+++ app/models/discovery_type.rb
@@ -1,6 +1,6 @@
 class DiscoveryType < ActiveRecord::Base
 
-	ORDERED_NAME = ["Facebook", "Linkedin", "Search Engine", "Friend of Buynance", "Other"]
+	ORDERED_NAME = ["Facebook", "Linkedin", "Search Engine", "Family of Buynance", "Other"]
 
 	def self.get_all_ordered
 		ordered = Array.new
