0683eaaffe794473ec4d8a8e08fc0fc74957601b
Edwin Velasquez <edwin.,velaquez@gmail.com>
2014-11-11 03:49:51 -0500

    New anonymous csv and transaction list

--- app/admin/transaction.rb
+++ app/admin/transaction.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register Transaction do
 
+	index do
+	end
+	
 	csv do
 		column("Date", :sortable => :transaction_date) {|transaction| transaction.transaction_date.strftime("%m/%d/%Y")}
         column("Amount", :sortable => :amount)         {|transaction| ActionController::Base.helpers.number_to_currency transaction.amount}
